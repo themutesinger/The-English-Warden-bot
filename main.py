be2c37507ab9ef541c3d1a642e2aa6102ff8525f
Suvan <overmind77s@gmail.com>
2022-11-16 05:22:45 +0600

    pep8

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ def main():
     updater = Updater(token=TOKEN)
     dp = updater.dispatcher
 
-
     reply_handler = MessageHandler(Filters.text, reply)
 
-
     dp.add_handler(reply_handler)
-    
+
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
